1d4eb25e22ba32dc4971bd9b73ff70d72f23f00f
elnir-elnir <elnir@ya.ru>
2026-06-14 23:30:56 +0700

    1. Исправлена опечатка в названии метода return_to_groups_page (с proups на groups) в файле test_add_group 2. Создан класс Contact 3. В файле test_add_contact.py - добавлены вспомогательные методы, - исправлены локаторы для полей bmonth, amonth, new_group в тесте create_new_contact.py

--- test_add_group.py
+++ test_add_group.py
@@ -20,7 +20,7 @@ class TestAddGroup(unittest.TestCase):
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
         self.create_group(wd, Group(name="dfgdfgdfg", header="dfgdfg", footer="dfgfgd"))
-        self.return_to_proups_page(wd)
+        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
@@ -30,13 +30,13 @@ class TestAddGroup(unittest.TestCase):
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_proups_page(wd)
+        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def logout(self, wd: WebDriver):
         wd.find_element(By.LINK_TEXT, "Logout").click()
 
-    def return_to_proups_page(self, wd: WebDriver):
+    def return_to_groups_page(self, wd: WebDriver):
         wd.find_element(By.LINK_TEXT, "group page").click()
 
     def create_group(self, wd: WebDriver, group):
