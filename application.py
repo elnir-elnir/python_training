b8088ce4702b5d5c51e3c61f8d7793afe3cf567c
elnir-elnir <elnir@ya.ru>
2026-06-15 22:18:28 +0700

    В соответствии с лекцией 2 раздела 2 1. Доработаны тестовые методы - получение ссылки на веб-драйвер перенесено из тестового метода во вспомогательные методы. 2. Создан файл application.py 3. Вспомогательные методы перенесены из тестовых классов во вспомогательный класс Application 4. В тестовых классах выполнен импорт pytest, а неиспользуемый импорт удален 5. Выполнен переход с unittest на pytest: тестовые классы удалены, тесты стали обычными функциями, в которые фикстура передается в качестве параметра

---
+++ application.py
@@ -0,0 +1,131 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+class Application:
+
+    def __init__(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def logout(self):
+        wd = self.wd
+        wd.find_element(By.LINK_TEXT, "Logout").click()
+
+    def return_to_groups_page(self):
+        wd = self.wd
+        wd.find_element(By.LINK_TEXT, "group page").click()
+
+    def create_group(self, group):
+        wd = self.wd
+        # open groups page replaced from tests to create_group method
+        self.open_groups_page()
+        # init group creation
+        wd.find_element(By.NAME, "new").click()
+        # fill group firm
+        wd.find_element(By.NAME, "group_name").click()
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys(group.name)
+        wd.find_element(By.NAME, "group_header").click()
+        wd.find_element(By.NAME, "group_header").clear()
+        wd.find_element(By.NAME, "group_header").send_keys(group.header)
+        wd.find_element(By.NAME, "group_footer").click()
+        wd.find_element(By.NAME, "group_footer").clear()
+        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        # submit group creation
+        wd.find_element(By.NAME, "submit").click()
+        # return to groups page replaced from tests to create_group method
+        self.return_to_groups_page()
+
+    def create_new_contact(self, contact):
+        wd = self.wd
+        wd.find_element(By.LINK_TEXT, "add new").click()
+        wd.find_element(By.NAME, "firstname").click()
+        wd.find_element(By.NAME, "firstname").clear()
+        wd.find_element(By.NAME, "firstname").send_keys(contact.firstname)
+        wd.find_element(By.NAME, "middlename").click()
+        wd.find_element(By.NAME, "middlename").clear()
+        wd.find_element(By.NAME, "middlename").send_keys(contact.middlename)
+        wd.find_element(By.NAME, "lastname").click()
+        wd.find_element(By.NAME, "lastname").clear()
+        wd.find_element(By.NAME, "lastname").send_keys(contact.lastname)
+        wd.find_element(By.NAME, "nickname").click()
+        wd.find_element(By.NAME, "nickname").clear()
+        wd.find_element(By.NAME, "nickname").send_keys(contact.nickname)
+        wd.find_element(By.NAME, "title").click()
+        wd.find_element(By.NAME, "title").clear()
+        wd.find_element(By.NAME, "title").send_keys(contact.title)
+        wd.find_element(By.NAME, "company").click()
+        wd.find_element(By.NAME, "company").clear()
+        wd.find_element(By.NAME, "company").send_keys(contact.company)
+        wd.find_element(By.NAME, "address").click()
+        wd.find_element(By.NAME, "address").clear()
+        wd.find_element(By.NAME, "address").send_keys(contact.address)
+        wd.find_element(By.NAME, "home").click()
+        wd.find_element(By.NAME, "home").clear()
+        wd.find_element(By.NAME, "home").send_keys(contact.home_phone)
+        wd.find_element(By.NAME, "mobile").click()
+        wd.find_element(By.NAME, "mobile").clear()
+        wd.find_element(By.NAME, "mobile").send_keys(contact.mobile_phone)
+        wd.find_element(By.NAME, "work").click()
+        wd.find_element(By.NAME, "work").clear()
+        wd.find_element(By.NAME, "work").send_keys(contact.work_phone)
+        wd.find_element(By.NAME, "email").click()
+        wd.find_element(By.NAME, "email").clear()
+        wd.find_element(By.NAME, "email").send_keys(contact.email)
+        wd.find_element(By.NAME, "email2").click()
+        wd.find_element(By.NAME, "email2").clear()
+        wd.find_element(By.NAME, "email2").send_keys(contact.email2)
+        wd.find_element(By.NAME, "email3").click()
+        wd.find_element(By.NAME, "email3").clear()
+        wd.find_element(By.NAME, "email3").send_keys(contact.email3)
+        wd.find_element(By.NAME, "homepage").click()
+        wd.find_element(By.NAME, "homepage").clear()
+        wd.find_element(By.NAME, "homepage").send_keys(contact.homepage)
+        wd.find_element(By.NAME, "bday").click()
+        Select(wd.find_element(By.NAME, "bday")).select_by_visible_text(contact.bday)
+        wd.find_element(By.XPATH, f"//option[@value='{contact.bday}']").click()
+        wd.find_element(By.NAME, "bmonth").click()
+        Select(wd.find_element(By.NAME, "bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element(By.XPATH, f"//option[@value='{contact.bmonth}']").click()
+        wd.find_element(By.NAME, "byear").click()
+        wd.find_element(By.NAME, "byear").clear()
+        wd.find_element(By.NAME, "byear").send_keys(contact.byear)
+        wd.find_element(By.NAME, "aday").click()
+        Select(wd.find_element(By.NAME, "aday")).select_by_visible_text(contact.aday)
+        # code line from recorder has been fixed to its one
+        #wd.find_element(By.XPATH, "//div[@id='content']/form/select[3]/option[4]").click()
+        wd.find_element(By.CSS_SELECTOR, f'select[name="aday"] > option[value="{contact.aday}"]').click()
+        wd.find_element(By.NAME, "amonth").click()
+        Select(wd.find_element(By.NAME, "amonth")).select_by_visible_text(contact.amonth)
+        # code line from recorder has been fixed to its one
+        #wd.find_element(By.XPATH, "//div[@id='content']/form/select[4]/option[3]").click()
+        wd.find_element(By.CSS_SELECTOR, f'select[name="amonth"] > option[value="{contact.amonth}"]').click()
+        wd.find_element(By.NAME, "ayear").click()
+        wd.find_element(By.NAME, "ayear").clear()
+        wd.find_element(By.NAME, "ayear").send_keys(contact.ayear)
+        wd.find_element(By.NAME, "new_group").click()
+        Select(wd.find_element(By.NAME, "new_group")).select_by_visible_text(contact.new_group)
+        wd.find_element(By.CSS_SELECTOR, f'select[name=new_group] > option[value="{contact.new_group}"]').click()
+        wd.find_element(By.XPATH, "//div[@id='content']/form/input[19]").click()
+
+    def open_groups_page(self):
+        wd = self.wd
+        wd.find_element(By.LINK_TEXT, "groups").click()
+
+    def login(self, password, username):
+        wd = self.wd
+        self.open_home_page()
+        wd.find_element(By.NAME, "user").click()
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys(username)
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys(password)
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+
+    def destroy(self):
+        self.wd.quit()
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,139 +1,35 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+
+import pytest
 from contact import Contact
+from application import Application
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
 
-    def test_add_contact(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_new_contact(wd, Contact(firstname="first", middlename="middle", lastname="last", nickname="nick",
-                                            title="title", company="comp", address="address", home_phone="123-456",
-                                            mobile_phone="+71234567890", work_phone="789-000", email="edc@ya.ru",
-                                            email2="edc@mail.ru", email3="edc@gmail.com", homepage="edc\\rfv.ru",
-                                            bday="1", bmonth="January", byear="2000", aday="2", amonth="February",
-                                            ayear="2020", new_group="[none]"))
-        self.logout(wd)
+# method for fixture initialization
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    # indication of how the fixture should be destroyed
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_add_empty_contact(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_new_contact(wd, Contact(firstname="", middlename="", lastname="", nickname="",
-                                            title="", company="", address="", home_phone="",
-                                            mobile_phone="", work_phone="", email="",
-                                            email2="", email3="", homepage="",
-                                            bday="", bmonth="-", byear="", aday="", amonth="-",
-                                            ayear="", new_group="[none]"))
-        self.logout(wd)
 
-    def logout(self, wd: WebDriver):
-        wd.find_element(By.LINK_TEXT, "Logout").click()
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.create_new_contact(Contact(firstname="first", middlename="middle", lastname="last", nickname="nick",
+                                        title="title", company="comp", address="address", home_phone="123-456",
+                                        mobile_phone="+71234567890", work_phone="789-000", email="edc@ya.ru",
+                                        email2="edc@mail.ru", email3="edc@gmail.com", homepage="edc\\rfv.ru",
+                                        bday="1", bmonth="January", byear="2000", aday="2", amonth="February",
+                                        ayear="2020", new_group="[none]"))
+    app.logout()
 
-    def create_new_contact(self, wd: WebDriver, contact):
-        wd.find_element(By.LINK_TEXT, "add new").click()
-        wd.find_element(By.NAME, "firstname").click()
-        wd.find_element(By.NAME, "firstname").clear()
-        wd.find_element(By.NAME, "firstname").send_keys(contact.firstname)
-        wd.find_element(By.NAME, "middlename").click()
-        wd.find_element(By.NAME, "middlename").clear()
-        wd.find_element(By.NAME, "middlename").send_keys(contact.middlename)
-        wd.find_element(By.NAME, "lastname").click()
-        wd.find_element(By.NAME, "lastname").clear()
-        wd.find_element(By.NAME, "lastname").send_keys(contact.lastname)
-        wd.find_element(By.NAME, "nickname").click()
-        wd.find_element(By.NAME, "nickname").clear()
-        wd.find_element(By.NAME, "nickname").send_keys(contact.nickname)
-        wd.find_element(By.NAME, "title").click()
-        wd.find_element(By.NAME, "title").clear()
-        wd.find_element(By.NAME, "title").send_keys(contact.title)
-        wd.find_element(By.NAME, "company").click()
-        wd.find_element(By.NAME, "company").clear()
-        wd.find_element(By.NAME, "company").send_keys(contact.company)
-        wd.find_element(By.NAME, "address").click()
-        wd.find_element(By.NAME, "address").clear()
-        wd.find_element(By.NAME, "address").send_keys(contact.address)
-        wd.find_element(By.NAME, "home").click()
-        wd.find_element(By.NAME, "home").clear()
-        wd.find_element(By.NAME, "home").send_keys(contact.home_phone)
-        wd.find_element(By.NAME, "mobile").click()
-        wd.find_element(By.NAME, "mobile").clear()
-        wd.find_element(By.NAME, "mobile").send_keys(contact.mobile_phone)
-        wd.find_element(By.NAME, "work").click()
-        wd.find_element(By.NAME, "work").clear()
-        wd.find_element(By.NAME, "work").send_keys(contact.work_phone)
-        wd.find_element(By.NAME, "email").click()
-        wd.find_element(By.NAME, "email").clear()
-        wd.find_element(By.NAME, "email").send_keys(contact.email)
-        wd.find_element(By.NAME, "email2").click()
-        wd.find_element(By.NAME, "email2").clear()
-        wd.find_element(By.NAME, "email2").send_keys(contact.email2)
-        wd.find_element(By.NAME, "email3").click()
-        wd.find_element(By.NAME, "email3").clear()
-        wd.find_element(By.NAME, "email3").send_keys(contact.email3)
-        wd.find_element(By.NAME, "homepage").click()
-        wd.find_element(By.NAME, "homepage").clear()
-        wd.find_element(By.NAME, "homepage").send_keys(contact.homepage)
-        wd.find_element(By.NAME, "bday").click()
-        Select(wd.find_element(By.NAME, "bday")).select_by_visible_text(contact.bday)
-        wd.find_element(By.XPATH, f"//option[@value='{contact.bday}']").click()
-        wd.find_element(By.NAME, "bmonth").click()
-        Select(wd.find_element(By.NAME, "bmonth")).select_by_visible_text(contact.bmonth)
-        wd.find_element(By.XPATH, f"//option[@value='{contact.bmonth}']").click()
-        wd.find_element(By.NAME, "byear").click()
-        wd.find_element(By.NAME, "byear").clear()
-        wd.find_element(By.NAME, "byear").send_keys(contact.byear)
-        wd.find_element(By.NAME, "aday").click()
-        Select(wd.find_element(By.NAME, "aday")).select_by_visible_text(contact.aday)
-        # code line from recorder has been fixed to its one
-        #wd.find_element(By.XPATH, "//div[@id='content']/form/select[3]/option[4]").click()
-        wd.find_element(By.CSS_SELECTOR, f'select[name="aday"] > option[value="{contact.aday}"]').click()
-        wd.find_element(By.NAME, "amonth").click()
-        Select(wd.find_element(By.NAME, "amonth")).select_by_visible_text(contact.amonth)
-        # code line from recorder has been fixed to its one
-        #wd.find_element(By.XPATH, "//div[@id='content']/form/select[4]/option[3]").click()
-        wd.find_element(By.CSS_SELECTOR, f'select[name="amonth"] > option[value="{contact.amonth}"]').click()
-        wd.find_element(By.NAME, "ayear").click()
-        wd.find_element(By.NAME, "ayear").clear()
-        wd.find_element(By.NAME, "ayear").send_keys(contact.ayear)
-        wd.find_element(By.NAME, "new_group").click()
-        Select(wd.find_element(By.NAME, "new_group")).select_by_visible_text(contact.new_group)
-        wd.find_element(By.CSS_SELECTOR, f'select[name=new_group] > option[value="{contact.new_group}"]').click()
-        wd.find_element(By.XPATH, "//div[@id='content']/form/input[19]").click()
-
-    def login(self, wd: WebDriver, username, password):
-        self.open_home_page(wd)
-        wd.find_element(By.NAME, "user").click()
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys(username)
-        wd.find_element(By.NAME, "pass").click()
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys(password)
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
-
-    def open_home_page(self, wd: WebDriver):
-        wd.get("http://localhost/addressbook/")
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_empty_contact(app):
+    app.login(username="admin", password="secret")
+    app.create_new_contact(Contact(firstname="", middlename="", lastname="", nickname="",
+                                        title="", company="", address="", home_phone="",
+                                        mobile_phone="", work_phone="", email="",
+                                        email2="", email3="", homepage="",
+                                        bday="", bmonth="-", byear="", aday="", amonth="-",
+                                        ayear="", new_group="[none]"))
+    app.logout()
