279c1753ced47d9ef29d505fce11d6f9fc999d38
elnir-elnir <elnir@ya.ru>
2026-06-16 00:31:41 +0700

    В соответствии с лекцией 3 раздела 2:  - выделен вспомогательный класс ContactHelper для работы с контактами,  - переименован метод create_new_contact в create

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -16,20 +16,20 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_new_contact(Contact(firstname="first", middlename="middle", lastname="last", nickname="nick",
-                                        title="title", company="comp", address="address", home_phone="123-456",
-                                        mobile_phone="+71234567890", work_phone="789-000", email="edc@ya.ru",
-                                        email2="edc@mail.ru", email3="edc@gmail.com", homepage="edc\\rfv.ru",
-                                        bday="1", bmonth="January", byear="2000", aday="2", amonth="February",
-                                        ayear="2020", new_group="[none]"))
+    app.contact.create(Contact(firstname="first", middlename="middle", lastname="last", nickname="nick",
+                               title="title", company="comp", address="address", home_phone="123-456",
+                               mobile_phone="+71234567890", work_phone="789-000", email="edc@ya.ru",
+                               email2="edc@mail.ru", email3="edc@gmail.com", homepage="edc\\rfv.ru",
+                               bday="1", bmonth="January", byear="2000", aday="2", amonth="February",
+                               ayear="2020", new_group="[none]"))
     app.session.logout()
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_new_contact(Contact(firstname="", middlename="", lastname="", nickname="",
-                                        title="", company="", address="", home_phone="",
-                                        mobile_phone="", work_phone="", email="",
-                                        email2="", email3="", homepage="",
-                                        bday="", bmonth="-", byear="", aday="", amonth="-",
-                                        ayear="", new_group="[none]"))
+    app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="",
+                               title="", company="", address="", home_phone="",
+                               mobile_phone="", work_phone="", email="",
+                               email2="", email3="", homepage="",
+                               bday="", bmonth="-", byear="", aday="", amonth="-",
+                               ayear="", new_group="[none]"))
     app.session.logout()
