13e83ff1e394ca5787b5378c8e30fe2242270c11
elnir-elnir <elnir@ya.ru>
2026-07-07 00:52:09 +0700

    К домашней работе № 9 В тесты модификации групп добавлена проверка предусловий (наличие групп, контактов)

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,26 +3,54 @@
 # description:
 #------------------------------------------------------------------------------
 
+from model.group import Group
+
+
 
 # Methods app.user.login() and app.session.logout() have been removed from
 # all tests because fixture have been optimized (lesson 3-3)
 
 def test_full_modify_new_group_initial_without_contacts(app):
-    group = app.data.create_custom_group()
-    app.data.full_modified_group(group)
+    # Добавляем проверку наличия группы и создание группы, если группы нет (урок 3-5)
+    group_name = "test"
+    if app.group.count_group_by_name(group_name) == 0:
+        app.group.create(Group(name=group_name))
+    # Добавляем проверку наличия в группе контактов (урок 3-5)
+    if app.group.count_of_contacts_in_group(group_name) > 0:
+        app.contact.select_all_contacts()
+        app.contact.delete_contact_from_contact_list()
+
+    app.group.open_groups_page()
+    app.group.select_group_by_name(group_name)
+    app.group.full_modify_group_by_name(group_name, new_group_name="modified_test_group", new_group_header="modified_test_header", new_group_footer="modified_test_footer")
 
 
 
 def test_confirm_new_group_modify_without_changes_when_group_has_no_contacts(app):
-    group_name = app.data.create_custom_group().name
+    # Добавляем проверку наличия группы и создание группы, если группы нет (урок 3-5)
+    group_name = "test"
+    if app.group.count_group_by_name(group_name) == 0:
+        app.group.create(Group(name=group_name))
+    # Добавляем проверку наличия в группе контактов (урок 3-5)
+    if app.group.count_of_contacts_in_group(group_name) > 0:
+        app.contact.select_all_contacts()
+        app.contact.delete_contact_from_contact_list()
+
     app.group.open_and_confirm_group_modify_without_changes_by_name(group_name)
 
 
 
 def test_full_modify_new_group_initial_with_one_contact(app):
-    group = app.data.create_custom_group()
-    app.data.create_contact_with_custom_group(group.name)
-    app.data.full_modified_group(group)
+    # Добавляем проверку наличия группы и создание группы, если группы нет (урок 3-5)
+    group_name = "test"
+    if app.group.count_group_by_name(group_name) == 0:
+        app.group.create(Group(name=group_name))
+    # Добавляем проверку наличия в группе контактов (урок 3-5)
+    if app.group.count_of_contacts_in_group(group_name) == 0:
+        app.data.create_contact_with_custom_group(group_name)
+
+    app.group.open_groups_page()
+    app.group.full_modify_group_by_name(group_name, new_group_name="modified_test_group", new_group_header="modified_test_header", new_group_footer="modified_test_footer")
 
 
 
--- test/test_swt_modify_group.py
+++ test/test_swt_modify_group.py
@@ -2,15 +2,25 @@
 # qa:
 # description: Тесты в соответствии с уроком 3-2
 #------------------------------------------------------------------------------
+
 from model.group import Group
 
 
 # Methods app.session.login() and app.session.logout() have been removed from
 # all tests because fixture have been optimized (lesson 3-3)
 def test_modify_first_group_name(app):
+    # Добавляем проверку наличия группы и создание группы, если группы нет (урок 3-5)
+    group_name = "test"
+    if app.group.count_group_by_name(group_name) == 0:
+        app.group.create(Group(name=group_name))
+
     app.group.modify_first_group(Group(name="New group"))
 
 
 
 def test_modify_first_group_header(app):
+    # Добавляем проверку наличия группы и создание группы, если группы нет (урок 3-5)
+    if app.group.count() == 0:
+        app.group.create(Group(header="Header"))
+
     app.group.modify_first_group(Group(header="New header"))
