7cdd1233ab74c2f66918eadcdea94e58a516ddd3
elnir-elnir <elnir@ya.ru>
2026-07-24 20:08:56 +0700

    1. Добавлена генерация случайных тестовых данных: - 1 группа с пустыми полями + 1 группа с заполненными полями; - 1 группа с пустыми полями + 5 (для примера) групп с заполненными полями; - с разными комбинациями заполненности полей

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,17 +2,58 @@
 # qa:
 # description:
 #------------------------------------------------------------------------------
+import random
+import string
+
 import pytest
 
 from model.group import Group
 
 
 
+# Добавлен вспомогательный метод для генерации случайных тестовых данных (урок 5-7)
+# В качестве параметров передается префикс и параметр, определяющий максимальную длину генерируемой
+# строки (урок 5-7)
+# " "*10 - увеличение количества символов пробела для повышения вероятности включения пробела в
+# сгенерированную строку
+def random_string(prefix, maxlen):
+    # Определяем символы, которые мы будем использовать для генерации тестовых данных (урок 5-7)
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    # Случайным образом выбираем символ из заданной строки и генерируем данные случайно длины, не
+    # превышающей максимальную длину. Склиеваем символы в одну строку (урок 5-7)
+    return prefix + "".join([random.choice(symbols) for  i in range(random.randrange(maxlen))])
+
+
+
+
 # Добавляем тестовые данные, определенные вне тестов, для параметризации тестов
 # чтобы тестовые данные передавались в тестовую функцию в качестве параметра (урок 5-7)
-testdata = [
-    Group(name="scasc", header="fdbvd", footer="dsvb"),
+# В виде фиксированного списка (урок 5-7) - закоментировано после добавления метода генерации
+# тестовых данных
+# testdata = [
+#     Group(name="scasc", header="fdbvd", footer="dsvb"),
+#     Group(name="", header="", footer="")
+# ]
+# В виде сгенерированных тестовых данных (урок 5-7)
+# Вариант 1: список из 1 пустой группы и 1 группы с заполненными полями
+testdata1 = [
+    Group(name=random_string("name", 10), header=random_string("header", 20),\
+          footer=random_string("footer", 20)),
     Group(name="", header="", footer="")
+]
+
+# Вариант 2: 1 группа с пустыми полями + 5 (для примера) групп с заполненными полями
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name", 10), header=random_string("header", 20),\
+          footer=random_string("footer", 20)) for i in range(5)
+]
+
+# Вариант 3: с разными комбинациями заполненности полей (полный перебор возможных случаев)
+testdata3 = [
+    Group(name=name, header=header, footer=footer)
+    for name in ["", random_string("name", 10)]
+    for header in ["", random_string("header", 20)]
+    for footer in ["", random_string("footer", 20)]
 ]
 
 
