ed1a6fa2d0497059bddd1d985042cf4ee8b7c17e
elnir-elnir <elnir@ya.ru>
2026-07-26 21:29:32 +0700

    Урок 6-9 Отделение тестовых данных от сценариев 1. Создан пакет data с файлом add_group.py, в который перенесены методы генерации тестовых данных из файла test_add_group.py 2. Добавлен список фиксированных тестовых данных - constant

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,64 +2,22 @@
 # qa:
 # description:
 #------------------------------------------------------------------------------
-import random
-import string
 
 import pytest
 
 from model.group import Group
 
+# Импорт сгенерированных случайных данных (урок 6-9)
+from data.add_group import testdata
+from data.add_group import testdata1
+from data.add_group import testdata3
+
+# # Импорт фиксированных данных (например, для отладки тестов) (урок 6-9)
+# from data.add_group import constant as testdata
 
 
-# Добавлен вспомогательный метод для генерации случайных тестовых данных (урок 5-7)
-# В качестве параметров передается префикс и параметр, определяющий максимальную длину генерируемой
-# строки (урок 5-7)
-# " "*10 - увеличение количества символов пробела для повышения вероятности включения пробела в
-# сгенерированную строку
-def random_string(prefix, maxlen):
-    # Определяем символы, которые мы будем использовать для генерации тестовых данных (урок 5-7)
-    # Временно убрала string.punctuation и " " * 10 (чтобы тесты не падали из-за "`" и " "
-    # во время их отладки в рамках выполнения дз 15 и дз 16)
-    #symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    symbols = string.ascii_letters + string.digits + " "
-    # Случайным образом выбираем символ из заданной строки и генерируем данные случайно длины, не
-    # превышающей максимальную длину. Склиеваем символы в одну строку (урок 5-7)
-    return prefix + "".join([random.choice(symbols) for  i in range(random.randrange(maxlen))])
-
-
-
-# Добавляем тестовые данные, определенные вне тестов, для параметризации тестов
-# чтобы тестовые данные передавались в тестовую функцию в качестве параметра (урок 5-7)
-# В виде фиксированного списка (урок 5-7) - закоментировано после добавления метода генерации
-# тестовых данных
-# testdata = [
-#     Group(name="scasc", header="fdbvd", footer="dsvb"),
-#     Group(name="", header="", footer="")
-# ]
-# В виде сгенерированных тестовых данных (урок 5-7)
-# Вариант 1: список из 1 пустой группы и 1 группы с заполненными полями
-testdata1 = [
-    Group(name=random_string("name", 10), header=random_string("header", 20),\
-          footer=random_string("footer", 20)),
-    Group(name="", header="", footer="")
-]
-
-
-# Вариант 2: 1 группа с пустыми полями + 5 (для примера) групп с заполненными полями
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20),\
-          footer=random_string("footer", 20)) for i in range(5)
-]
-
-
-# Вариант 3: с разными комбинациями заполненности полей (полный перебор возможных случаев)
-testdata3 = [
-    Group(name=name, header=header, footer=footer)
-    for name in ["", random_string("name", 10)]
-    for header in ["", random_string("header", 20)]
-    for footer in ["", random_string("footer", 20)]
-]
-
+# Методы генерации тестовых данных random_string и testdata, testdata1, testdata3, составленные
+# в рамках урока 5-7, перенесены из этого файла в файл add_group.py (урок 6-9)
 
 
 # Methods app.session.login() and app.session.logout() have been removed from
